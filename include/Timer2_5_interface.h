f115a18decf2b7e3bfe67282e3e9d4ab31a97e5a
Ahmed Salah <ahmedsalahalanani@gmail.com>
2020-09-15 00:42:57 +0200

    Add files via upload

---
+++ include/Timer2_5_interface.h
@@ -0,0 +1,27 @@
+/******************************************************************/
+/* Author	: Ahmed Salah										  */
+/* Date		: 13 Sep 2020										  */
+/* Version	: V01												  */
+/******************************************************************/
+#ifndef Timer2_5_INTERFACE_H
+#define Timer2_5_INTERFACE_H
+
+
+/*
+*********************************************************************************************************
+*                                           FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+void MTimer2_voidInit (void);
+void MTimer2_voidSetBusyWait(u16 Copy_u16Ticks);
+
+void MTimer3_voidInit (void);
+void MTimer3_voidSetBusyWait(u16 Copy_u16Ticks);
+
+void MTimer4_voidInit (void);
+void MTimer4_voidSetBusyWait(u16 Copy_u16Ticks);
+
+void MTimer5_voidInit (void);
+void MTimer5_voidSetBusyWait(u16 Copy_u16Ticks);
+#endif
+//***************************************************************************************************************
